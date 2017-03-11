63f9b5f0ed93b74e846a319d2c253d298437bd8f
junwon1994 <junwon1994@gmail.com>
2017-03-11 14:27:40 +0900

    매우 조그마한 수정

--- algorithm-game-app/src/app/app.component.ts
+++ algorithm-game-app/src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
+const Games = [
+  { detail: "첫 번째 게임 설명", image: "첫 번째 사진" },
+  { detail: "두 번째 게임 설명", image: "두 번째 사진" },
+  { detail: "세 번째 게임 설명", image: "세 번째 사진" },
+  { detail: "네 번째 게임 설명", image: "네 번째 사진" }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +15,10 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class AppComponent {
   items: FirebaseListObservable<any[]>;
+  games;
 
   constructor(af: AngularFire) {
     this.items = af.database.list('/server/member/mory/keyvalue/separatedKeys');
+    this.games = Games;
   }
 }

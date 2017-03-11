c5c3abd7837ee08b7f3333f344c733d09552929f
junwon1994 <junwon1994@gmail.com>
2017-03-11 10:55:30 +0900

    클라이언트 업로드 및 깃 무시 목록 업데이트

---
+++ algorithm-game-app/src/environments/firebase-config.ts
@@ -0,0 +1,14 @@
+import { FirebaseAppConfig } from 'angularfire2';
+
+/**
+ * Firebase App Configuration
+ * TODO: Seperate config code to another module
+ */
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: 'AIzaSyCkA9kuLMSMpoYwqcF0ZWLYmboYkVURc1M',
+  authDomain: 'm2mproject-d7ae6.firebaseapp.com',
+  databaseURL: 'https://m2mproject-d7ae6.firebaseio.com',
+  storageBucket: 'm2mproject-d7ae6.appspot.com',
+  messagingSenderId: '876511931679'
+};
+

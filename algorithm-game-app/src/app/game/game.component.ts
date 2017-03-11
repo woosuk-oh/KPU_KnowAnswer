43254a91b2975da5134deacb53151327a5197f9f
junwon1994 <junwon1994@gmail.com>
2017-03-11 17:09:01 +0900

    살짝 추가

--- algorithm-game-app/src/app/game/game.component.ts
+++ algorithm-game-app/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location
+  ) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
